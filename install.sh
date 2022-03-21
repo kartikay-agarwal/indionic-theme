4c5813f38909a8c542d8e443558b8d0f0f748ba8
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-03-21 22:34:37 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+cd /var/www/pterodactyl
+
+wget https://raw.githubusercontent.com/kartikay-agarwal/indionic-theme/main/theme.zip
+
+rm -R app
+rm -R resources
+rm -R routes
+rm -R database
+rm -R tailwind.config.js
+
+apt -y install unzip
+unzip theme.zip
+
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+
+npm install -g yarn
+yarn install 
+yarn build:production
+
+php artisan route:clear && php artisan cache:clear && php artisan view:clear && php artisan migrate
