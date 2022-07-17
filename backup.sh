3b6a9da4266f673d2ace69fdc9eb2bf2cf93222a
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-07-17 15:07:05 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,19 +5,27 @@ THEDBUSER="root"
 THEDBPW="9913808057Aa@"
 
 # zip all files
-zip -r backup_of_${THEDATE}.zip /var/lib/pterodactyl/volumes -P YXdzIGtpIG1hYSBrYSBiaG9zZGE=
+#zip -r backup_of_${THEDATE}.zip /var/lib/pterodactyl/volumes -P YXdzIGtpIG1hYSBrYSBiaG9zZGE=
+
+cd /var/lib/pterodactyl/volumes
+for i in */; do zip -r "${i%/}.zip" "$i"; done
+
+for i in *.zip; do mv "${i%}" /var/www/_backups/ptero; done
+
+zip -r backup_of_${THEDATE}.zip /var/www/_backups/ptero -P YXdzIGtpIG1hYSBrYSBiaG9zZGE=
 
 # export all the db
+cd ~
 mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" > /var/www/_backups/${dbname}_${THEDATE}.sql; done
-zip -r /var/www/_backups/dbbackup_${THEDATE}.zip /var/www/_backups -P YXdzIGtpIG1hYSBrYSBiaG9zZGE=
+zip -r /var/www/_backups/dbbackup_${THEDATE}.zip /var/www/_backups/db -P YXdzIGtpIG1hYSBrYSBiaG9zZGE=
 #mysqldump -u ${THEDBUSER} -p${THEDBPW} --all-databases > /var/www/_backups/dbbackup_${THEDATE}.sql
 
 # move the zip and db
-mv backup_of_${THEDATE}.zip /var/lib/_backups/ptero
+mv /var/www/_backups/backup_of_${THEDATE}.zip /var/lib/_backups/ptero
 mv /var/www/_backups/dbbackup_${THEDATE}.zip /var/lib/_backups/db
 
 #clear the extra db backups
-rm -r /var/www/_backups && mkdir /var/www/_backups
+rm -r /var/www/_backups && mkdir /var/www/_backups/ptero && mkdir /var/www/_backups/db
 
 # Remove backups older than 3 days
 find /var/lib/_backups/ptero* -mtime +3 -exec rm {} \;
